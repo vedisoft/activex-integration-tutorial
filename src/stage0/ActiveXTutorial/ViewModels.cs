ecea4b15ce2a18abc5285eca19ef428843d94601
vedisoft <eugenea@MAGICCARPET.vedisoft.local>
2017-04-28 14:13:16 +0500

    stage0: remove unused using directives

--- src/stage0/ActiveXTutorial/ViewModels.cs
+++ src/stage0/ActiveXTutorial/ViewModels.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Globalization;
-using System.Text.RegularExpressions;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
 
 namespace ActiveXTutorial
 {
